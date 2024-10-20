571e0a00ea86a1602ce54b41084a75e814eb0b0d
Lstar974 <112543639+Lstar974@users.noreply.github.com>
2024-10-20 21:43:08 +0400

    Update and rename unit.test.js to counterIncrement.test.js

---
+++ test/counterIncrement.test.js
@@ -0,0 +1,26 @@
+const { JSDOM } = require('jsdom');
+
+test('Should increment the counter when button is clicked', () => {
+  const dom = new JSDOM(`
+    <html>
+      <body>
+        <p id="clickCounter">0</p>
+        <button onclick="incrementCounter()">Click Me!</button>
+      </body>
+    </html>
+  `);
+
+  const counter = dom.window.document.getElementById("clickCounter");
+
+  // Simule la fonction d'incrémentation
+  dom.window.incrementCounter = function() {
+    counter.innerText = parseInt(counter.innerText) + 1;
+  };
+
+  // Incrémentation simulée
+  dom.window.incrementCounter();
+  expect(counter.innerText).toBe("1");
+
+  dom.window.incrementCounter();
+  expect(counter.innerText).toBe("2");
+});
